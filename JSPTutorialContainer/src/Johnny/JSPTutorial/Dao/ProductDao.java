0eb4313ac0d940285ab84805b864e92823b366a0
Zhuang, Rong <jojozhuang@gmail.com>
2017-10-24 13:12:40 -0700

    jsptutorialcontainer

---
+++ JSPTutorialContainer/src/Johnny/JSPTutorial/Dao/ProductDao.java
@@ -0,0 +1,187 @@
+package Johnny.JSPTutorial.Dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import Johnny.JSPTutorial.Beans.Product;
+import Johnny.JSPTutorial.Database.ConnectionPool;
+import Johnny.JSPTutorial.Database.DBUtil;
+
+public class ProductDao {
+	public static int insert(Product product) {
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        PreparedStatement ps = null;
+        int generatedkey = 0;
+
+        String query
+                = "INSERT INTO Product (ProductName, Price) "
+                + "VALUES (?, ?)";
+        try {
+            connection.setAutoCommit(false); //transaction block start
+            ps = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, product.getProductName());
+            ps.setDouble(2, product.getPrice());
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+               generatedkey=rs.getInt(1);
+            }
+            
+            if (generatedkey > 0) {
+
+            } else {
+                connection.rollback();
+                return 0;
+            }
+            connection.commit(); //transaction block end
+            return generatedkey;
+        } catch (SQLException e) {
+            System.out.println(e);
+            return 0;
+        } finally {
+            DBUtil.closePreparedStatement(ps);
+            pool.freeConnection(connection);
+        }
+    }
+
+    public static int update(Product product) {
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        PreparedStatement ps = null;
+
+        String query = "UPDATE Product SET "
+                + "ProductName = ?, "
+                + "Price = ? "
+                + "WHERE ProductId = ?";
+        try {
+            connection.setAutoCommit(false); //transaction block start
+            ps = connection.prepareStatement(query);
+            ps.setString(1, product.getProductName());
+            ps.setDouble(2, product.getPrice());
+            ps.setInt(3, product.getProductId());
+            ps.executeUpdate();
+            connection.commit(); //transaction block end
+            return 1;
+        } catch (SQLException e) {
+            System.out.println(e);
+            return 0;
+        } finally {
+            DBUtil.closePreparedStatement(ps);
+            pool.freeConnection(connection);
+        }
+    }
+
+    public static int delete(int productid) {
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        PreparedStatement ps = null;
+
+        String query = "DELETE FROM Product "
+                + "WHERE ProductId = ?";
+        try {
+            connection.setAutoCommit(false); //transaction block start
+            ps = connection.prepareStatement(query);
+            ps.setInt(1, productid);
+            ps.executeUpdate();
+            connection.commit(); //transaction block end
+            return 1;
+        } catch (SQLException e) {
+            System.out.println(e);
+            return 0;
+        } finally {
+            DBUtil.closePreparedStatement(ps);
+            pool.freeConnection(connection);
+        }
+    }
+
+    public static boolean exists(int productid) {
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        String query = "SELECT ProductId FROM Product "
+                + "WHERE ProductId = ?";
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setInt(1, productid);
+            rs = ps.executeQuery();
+            return rs.next();
+        } catch (SQLException e) {
+            System.out.println(e);
+            return false;
+        } finally {
+            DBUtil.closeResultSet(rs);
+            DBUtil.closePreparedStatement(ps);
+            pool.freeConnection(connection);
+        }
+    }
+    
+    public static List<Product> getList() {
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        List<Product> list = new ArrayList();
+        
+        String query = "SELECT * FROM Product ";
+        try {
+            ps = connection.prepareStatement(query);
+            rs = ps.executeQuery();
+            Product product = null;
+            while (rs.next()) {
+                product = new Product();
+                product.setProductId(rs.getInt("ProductId"));
+                product.setProductName(rs.getString("ProductName"));
+                product.setPrice(rs.getDouble("Price"));
+                list.add(product);
+            }
+            return list;
+        } catch (SQLException e) {
+            System.out.println(e);
+            return null;
+        } finally {
+            DBUtil.closeResultSet(rs);
+            DBUtil.closePreparedStatement(ps);
+            pool.freeConnection(connection);
+        }
+    }
+    
+    public static Product getProduct(int id) {
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        String query = "SELECT * FROM Product "
+                + "WHERE ProductId = ?";
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+            Product product = null;
+            while (rs.next()) {
+                product = new Product();
+                product.setProductId(rs.getInt("ProductId"));
+                product.setProductName(rs.getString("ProductName"));
+                product.setPrice(rs.getDouble("Price"));
+                break;
+            }
+
+            return product;
+        } catch (SQLException e) {
+            System.out.println(e);
+            return null;
+        } finally {
+            DBUtil.closeResultSet(rs);
+            DBUtil.closePreparedStatement(ps);
+            pool.freeConnection(connection);
+        }
+    }
+}
