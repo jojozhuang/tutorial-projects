02e80b9e9bceffc03b19c307f66fe86fa732fecd
Johnny <jojozhuang@gmail.com>
2017-11-05 08:20:09 -0800

    jdb tutorial, sample program

---
+++ JDBTutorial/Sum.java
@@ -0,0 +1,8 @@
+public class Sum {
+    public static void main(String[] args){
+        int val1 = Integer.parseInt(args[0]);
+        int val2 = Integer.parseInt(args[1]);
+        int sum = val1 + val2;
+        System.out.println("Sum is " + sum);
+    }
+}
