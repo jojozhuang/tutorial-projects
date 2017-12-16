e7cde9cab876ff127e12a6459cc8790e588cf830
Zhuang, Rong <jojozhuang@gmail.com>
2017-12-15 21:27:22 -0800

    static web server

--- StaticWebServer/server.js
+++ StaticWebServer/server.js
@@ -5,7 +5,7 @@ var path = require('path');
 // view at http://localhost:12000/
 app.use('/', express.static(path.join(__dirname, '/')))
 // view at http://localhost:12000/2048/
-app.use('/2048', express.static(path.join(__dirname, '2048')))
+//app.use('/2048', express.static(path.join(__dirname, '2048')))
 
 app.listen(12000);
 console.log('Server running at http://localhost:12000/');
