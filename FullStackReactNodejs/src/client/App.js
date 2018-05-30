0f229bd211f43082c192eb3cdc9c5a92aa34e68d
Zhuang, Rong <jojozhuang@gmail.com>
2018-05-30 00:02:27 -0700

    full stack react node app

---
+++ FullStackReactNodejs/src/client/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './app.css';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { username: null };
+  }
+
+  componentDidMount() {
+    fetch('/api/getUsername')
+      .then(res => res.json())
+      .then(user => this.setState({ username: user.username }));
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.username ? (
+          <div><h1>Hello {this.state.username}</h1>
+          <p>This app is built with React and Node.js!</p></div>
+        ) : (
+          <h1>Loading.. please wait!</h1>
+        )}
+      </div>
+    );
+  }
+}
