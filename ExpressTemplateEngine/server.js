e2c2cba5f910b5406d8dc8eea064197ef449cbe5
Zhuang, Rong <jojozhuang@gmail.com>
2018-06-12 20:42:01 -0700

    express template engine

---
+++ ExpressTemplateEngine/server.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var path = require("path");
+var app = express();
+
+app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+  res.render("index", {
+    message: "Hey everyone! This is my webpage."
+  });
+});
+
+app.get("/about", function(req, res) {
+  res.render("about", {
+    about: "This website demonstrates how ejs works!"
+  });
+});
+
+app.listen(3000, function() {
+  console.log("Web Server started on port 3000");
+});
