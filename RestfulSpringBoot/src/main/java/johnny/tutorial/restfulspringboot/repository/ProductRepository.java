0bce423d7cbe52a1dcc3c95729afd86aabf029ca
Zhuang, Rong <jojozhuang@gmail.com>
2018-07-29 17:28:46 -0700

    delete RestfulSpringBoot, moved to https://github.com/jojozhuang/restful-api-springboot

--- RestfulSpringBoot/src/main/java/johnny/tutorial/restfulspringboot/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package johnny.tutorial.restfulspringboot.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import johnny.tutorial.restfulspringboot.domain.Product;
-
-public interface ProductRepository extends CrudRepository<Product, Long> {
-
-}
