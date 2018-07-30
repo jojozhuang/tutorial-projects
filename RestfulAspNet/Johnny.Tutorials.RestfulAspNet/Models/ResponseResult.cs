b8f5e6f9b1d143be724f1d6959b6bc88b9c52ef4
Zhuang, Rong <jojozhuang@gmail.com>
2018-07-29 17:11:16 -0700

    remove RestfulAspNet, move to https://github.com/jojozhuang/restful-api-aspnet

--- RestfulAspNet/Johnny.Tutorials.RestfulAspNet/Models/ResponseResult.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace Johnny.Tutorials.RestfulAspNet.Models
-{
-    public class ResponseResult
-    {
-        public int StatusCode { get; set; }
-        public string Message { get; set; }
-    }
-}
